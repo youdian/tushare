6cc5f88f348e3cc6d748feb6d6eaa68af1a613ea
shidong <shidong@yiran.com>
2016-06-08 15:05:39 +0800

    update history , add saveHistory method

--- history.py
+++ history.py
@@ -3,14 +3,21 @@ import sys
 from sql import engine
 import sql
 
-code = sys.argv[1]
+def saveHistory(code):
+	df = ts.get_hist_data(code)
+	table_name = 's_' + code
+	#先尝试清空表内容
+	try:
+		session = sql.db_session()
+		session.execute('delete from ' + table_name)
+	except:
+		pass
+	try:
+		df.to_sql(table_name,engine, if_exists='append')
+	except:
+		session.execute("alter table " + table_name + " change 'date' 'date' text character set utf8 collate utf8_general_ci NULL default null" )
+		session.close()
+		df.to_sql(table_name, engine, if_exists='append')
 
-df = ts.get_hist_data(code)
-table_name = 's_' + code
-try:
-	df.to_sql(table_name,engine, if_exists='append')
-except:
-	session = sql.db_session()
-	session.execute("alter table " + table_name + " change 'date' 'date' text character set utf8 collate utf8_general_ci NULL default null" )
-	session.close()
-	df.to_sql(table_name, engine, if_exists='append')
+code = sys.argv[1]
+getHistory(code)
