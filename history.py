6a307fe760b976b74a58ca3acb400a73319cfdd5
youdian <me@youdian.org>
2016-08-19 18:58:10 +0800

    rename files

--- history.py
+++ history.py
@@ -1,15 +1,33 @@
 import tushare as ts
-import sys
-from sql import engine
-import sql
+import os
+import pandas
+import time
 
-def saveHistory(code):
-	df = ts.get_hist_data(code)
-	if df is None:
-		print('df is none, code= %s' %code)
-		return
-	table_name = 'z_' + code
-	df.to_sql(table_name,engine, if_exists='append')
-if __name__ == '__main__':
-	code = sys.argv[1]
-	saveHistory(code)
+def fetch_history(stock):
+    df = ts.get_hist_data(stock)
+    if df is not None:
+        file_name = get_file_path(stock)
+        df.to_csv(file_name)
+        return df
+    else:
+        print(stock, 'fetch history failed')
+        return None
+
+
+def get_history(stock):
+    file_name = get_file_path(stock)
+    df = None
+    if not os.path.exists(file_name):
+        df = fetch_history(stock)
+    else:
+        df = pandas.read_csv(file_name, index_col=0)
+    return df
+
+def get_file_path(stock):
+    return 'csv/' + stock + '.csv'
+    
+def fetch_all():
+    df = pandas.read_csv('csv/stocks.csv', dtype={'code':str})
+    for code in df.code:
+        fetch_history(code)
+        time.sleep(0.1)    
