d711d58630addf03f0cb088a349aa3ef7bab3544
youdian <me@youdian.org>
2016-08-19 16:46:58 +0800

    add new file

---
+++ stock.py
@@ -0,0 +1,31 @@
+import tushare as ts
+import os
+import pandas
+import numpy as np
+
+def fetch_stock_list():
+    df = ts.get_stock_basics()
+    if df is not None:
+        df.to_csv(get_stock_path())
+    return df
+def get_stock_list():
+    path = get_stock_path()
+    df = None    
+    if os.path.exists(path):
+        df = pandas.read_csv(path,index_col='code', dtype={'code':np.object})
+        df.index = [format_code(code) for code in df.index]
+    else:
+        df = fetch_stock_list()
+    return df 
+
+def get_stock_path():
+    return 'csv/stocks.csv'
+
+def format_code(code):
+    length = len(str(code))
+    if length < 6:
+        s = '0' * (6 - length)
+        s += str(code)
+        return s
+    return str(code)
+
