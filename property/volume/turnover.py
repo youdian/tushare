81c2360865b170c811df00e8e0294c5da17d4778
youdian.org <android@youdian.org>
2017-03-21 21:00:08 +0800

    add turnover property

---
+++ property/volume/turnover.py
@@ -0,0 +1,17 @@
+'''the turnover of a stock on some day'''
+from property.property import BaseProperty
+
+class Turnover(BaseProperty):
+    '''the turnover of a stock'''
+    name = "turnover"
+    day = 0
+
+    def init(self, config):
+        self.day = config["day"] if config is not None else 0
+
+    def value(self):
+        return self._history[self.name][self.day]
+
+    def __repr__(self):
+        return self.name + " of day " + str(self.day) + \
+         " is " + str(self.value())
