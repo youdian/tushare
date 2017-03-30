7b54fb249662ed86cf27b092da2ba6316b477552
youdian.org <android@youdian.org>
2017-03-30 17:18:33 +0800

    merge remote

--- stock/history.py
+++ stock/history.py
@@ -7,9 +7,14 @@ import tushare as ts
 
 def fetch_history(stock):
     '''从服务器获取股票日行情'''
-    df = ts.get_k_data(code=stock, pause=0.01)
+    df = None
+    try:
+        df = ts.get_k_data(code=stock, pause=0.01)
+    except IndexError as err:
+        print("sotck ", stock, " fetch error:", str(err))
+        pass
     if df is not None:
-        add_percent(df)
+        # add_percent(df)
         file_name = get_file_path(stock)
         df.to_csv(file_name)
         return df
