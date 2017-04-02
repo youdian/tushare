66ae7985584503bdbdf1f34e0b9b53f64b624e8c
youdian.org <android@youdian.org>
2017-04-02 20:28:41 +0800

    tmp save

--- policy/hot.py
+++ policy/hot.py
@@ -1,10 +1,15 @@
 from stock import stock
 from stock import history
+from datetime import datetime
+from datetime import timedelta
+
 day = 10
 min_count = 20
 normal_p_change = 3
 min_p_change = 2.5
 min_p_high = 5
+last_trading_day = "2017-03-31"
+
 def get():
     stock_list = stock.get_stock_list()
     codes = stock_list.index
@@ -37,5 +42,25 @@ def k_close(kdata):
 def st(name):
     '''not a st stock'''
     st = "st"
-    valid = name.lower().startswith(st)
+    star_st = "*st";
+    valid = name.lower().startswith(st) or name.lower().startswith(star_st)
     return valid
+
+def updated(kdata):
+    date = kdata.date[len(kdata) - 1]
+    return date == lastTradingDate()
+
+def lastTradingDate():
+    now = datetime.now()
+    delta = 0
+    if now.weekday == 5:
+        delta = 1
+    elif now.weekday == 6:
+        delta = 2
+    elif now.hour < 16:
+        delta = 1
+    trading_day = now - timedelta(days=delta)
+    format = '%Y-%m-%d'
+    day_str = trading_day.strftime(format)
+    return day_str
+
